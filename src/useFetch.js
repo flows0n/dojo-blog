ca9c0f689b0cdc4408703c5a30faaf7a3217e2c5
LBPROL081\bmaliszewski <bmaliszewski@lbpro.pl>
2022-01-20 14:03:40 +0100

    Adding blog

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortCont = new AbortController();
+
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
         .then((res) => {
           if (!res.ok) {
             throw Error("Custom Error Message");
@@ -15,16 +17,22 @@ const useFetch = (url) => {
           return res.json();
         })
         .then((data) => {
-          console.log(data);
+          //console.log(data);
           setData(data);
           setIsPending(false);
           setError(null);
         })
         .catch((err) => {
-          setError(err.message);
-          setIsPending(false);
+          if (err.name === "AbortError") {
+            console.log("fetch abort");
+          } else {
+            setError(err.message);
+            setIsPending(false);
+          }
         });
     }, 1000);
+
+    return () => abortCont.abort();
   }, [url]);
 
   return { data, isPending, error };
