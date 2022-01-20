ecffcb4312c86729643e214fc8e018951663c39c
LBPROL081\bmaliszewski <bmaliszewski@lbpro.pl>
2022-01-20 14:34:53 +0100

    End

--- src/BlogList.js
+++ src/BlogList.js
@@ -5,12 +5,12 @@ const BlogList = ({ blogs, title }) => {
     <div className="blog-list">
       <h2>{title}</h2>
       {blogs.map((blog) => (
-        <Link to={`/blogs/${blog.id}`}>
-          <div className="blog-preview" key={blog.id}>
+        <div className="blog-preview" key={blog.id}>
+          <Link to={`/blogs/${blog.id}`}>
             <h2>{blog.title}</h2>
             <p>Written by {blog.author}</p>
-          </div>
-        </Link>
+          </Link>
+        </div>
       ))}
     </div>
   );
